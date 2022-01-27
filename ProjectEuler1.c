63f6233ac28399fe45df83f589a16fefaa9228e1
Brandon <brandon.streetsmcleod@gmail.com>
2022-01-27 11:06:23 +0000

    Problem 1 solved

--- ProjectEuler1.c
+++ ProjectEuler1.c
@@ -4,5 +4,20 @@
 
 int main (int argc , char ** argv )
 {
-	
+	int sum = 0;
+	int x = 999;
+	while (x > 0)
+	{
+		if (x%3 == 0 || x%5 == 0)
+		{
+			printf("%d\n", x);
+			sum = sum + x;
+			x--;
+		}
+		else
+		{
+			x--;
+		}
+	}
+	printf("%d", sum);
 }
